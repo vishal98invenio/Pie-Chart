f3fa7497f69b248c9bc564f0a4c26818c4b47344
Vishal Narang <vishal.narang@invenio-solutions.com>
2019-11-01 06:12:49 +0000

    Pie Chart

---
+++ webapp/controller/View1.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("Pie.PieChart.controller.View1", {
+		onInit: function () {
+
+		}
+	});
+});
